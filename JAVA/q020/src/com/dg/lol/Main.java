39cfa6c6766d0a5ffdbb22d60c29aeca09ee7088
kimaxee <95838445+kimaxee@users.noreply.github.com>
2022-01-13 18:19:06 +0900

    Add files via upload

---
+++ JAVA/q020/src/com/dg/lol/Main.java
@@ -0,0 +1,10 @@
+package com.dg.lol;
+
+import com.dg.lol.util.Dice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("주사위를 굴려" + Dice.getDice(6) + "이 나왔습니다.");
+	}
+}
