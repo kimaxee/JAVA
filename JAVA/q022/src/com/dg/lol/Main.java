a666a04d25cca92a5e46ce95efe09bc6ce19168a
kimaxee <95838445+kimaxee@users.noreply.github.com>
2022-01-14 11:31:28 +0900

    Add files via upload

---
+++ JAVA/q022/src/com/dg/lol/Main.java
@@ -0,0 +1,16 @@
+package com.dg.lol;
+
+import com.dg.lol.control.Game;
+import com.dg.lol.img.Entrance;
+import com.dg.lol.util.Dice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("주사위를 굴려" + Dice.getDice(6) + "이 나왔습니다.");
+		Game game = new Game();
+		game.start();
+		Entrance entrance = new Entrance();
+		entrance.Show();
+	}
+}
---
+++ JAVA/q022/src/com/dg/lol/img/Entrance.java
@@ -0,0 +1,12 @@
+package com.dg.lol.img;
+
+public class Entrance {
+
+	static final String ENTRANCE = " Legue of legend \n " + " 환영합니다 ";
+
+	public static void Show() {
+		System.out.println(ENTRANCE);
+
+	}
+
+}
